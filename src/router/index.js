50f0d3d50c93459c78639d1d3ae449a91bcf94b3
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-06-04 14:59:56 +0300

    feat: configure Vue Router with routes for home, about, programs, contact, and admissions pages

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ProgramsView from "../views/ProgramsView.vue";
+import ContactView from "../views/ContactView.vue";
+import ApplyView from "../views/ApplyView.vue";
+import TuitionView from "../views/TuitionView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/programs",
+    name: "programs",
+    component: ProgramsView,
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: ContactView,
+  },
+  {
+    path: "/admissions/apply",
+    name: "apply",
+    component: ApplyView,
+  },
+  {
+    path: "/admissions/tuition",
+    name: "tuition",
+    component: TuitionView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
