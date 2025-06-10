85e171a4affefc401ce9f8c4f6750897d35cfc78
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-06-10 15:16:13 +0300

    Add About section with Horizon Academy description and image

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,7 @@
           Our dedicated faculty and modern facilities create a supportive community where every student can thrive.
         </p>
       </div>
-      <img src="https://images.unsplash.com/photo-1541823709867-1b206113e9e9?ixlib=rb-4.0.3&auto=format&fit=crop&w=1350&q=80" alt="School Community" class="w-full h-80 object-cover rounded-xl shadow-lg" />
+      <img src="../assets/images/about.jpg" alt="School Community" class="w-full h-80 object-cover rounded-xl shadow-lg" />
     </div>
   </div>
 </template>
